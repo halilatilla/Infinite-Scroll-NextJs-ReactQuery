2f7be52d0bd4944d6415d786774f03cea45bd97a
halilatilla <atillahalil0@gmail.com>
2022-01-16 23:09:53 +0300

    Add Loading more animation

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as CharacterList } from './CharacterList/CharacterList'
 export { default as CharacterDetailCard } from './CharacterDetailCard/CharacterDetailCard'
+export { default as Loading } from './Loading/Loading'
